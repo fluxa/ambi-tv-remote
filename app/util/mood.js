1332baed0c6da4ef42d1e3b66fce97bf30af0fdf
Juan-Manuel Fluxà <jmfluxa@gmail.com>
2015-03-21 12:14:10 +0100

    new tweener engine

--- app/util/mood.js
+++ app/util/mood.js
@@ -6,6 +6,7 @@
 
 var lpd8806_lib = require('./lpd8806');
 var TWEEN = require('tween.js');
+var gameLoop = require('./gameLoop.js');
 
 var num_leds = 100;
 var device = '/dev/spidev0.0';
@@ -18,59 +19,30 @@ exports.off = function() {
 	}
 }
 
+var isColorBusy = false;
 exports.color = function(r,g,b) {
 	//console.log('fill color =>' + r + ',' + g + ',' + b);
-	if(lpd8806) {
-		lpd8806.fillRGB(r,g,b);
+	if (!isColorBusy) {
+		isColorBusy = true;
+		if(lpd8806) {
+			lpd8806.fillRGB(r,g,b);
+		}
+		isColorBusy = false;
 	}
 }
 
-
-// Animations
-var requestAnimationFrame = function(callback) {
-	var currTime = new Date().getTime();
-	var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-	var id = setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
-	lastTime = currTime + timeToCall;
-	return id;
-};
 
 var _tweener;
-var lastTime = 0;
-var _animInterval;
-var _animFrame;
-var animator = {
-	run : function(){
-		if( _animInterval == null ) {
-			_animInterval = setInterval(function(){
-				if(_tweener) {
-					_animFrame = requestAnimationFrame(function(time) {
-						TWEEN.update(time);
-					});
-				}
-			}, 1);
-			console.log('anim interval started');
-		} else {
-			console.log('anim is running');
-		}
-
-	},
-	stop : function(){
-		if(_animInterval) {
-			clearInterval(_animInterval);
-		}
-		_animInterval = null;
-		if(_animFrame) {
-			clearTimeout(_animFrame);
-		}
-		_animFrame = null;
-		console.log('anim interval stopped');
-	}
-
-}
-
 exports.colorTween = {
 	start : function(colors, time) {
+
+		// run loop
+		gameLoop.update = function(delta, now) {
+			TWEEN.update(now);
+		}
+		gameLoop.run();
+
+		// setup tweener
 		if(_tweener) {
 			_tweener.stop()
 		}
@@ -84,19 +56,21 @@ exports.colorTween = {
 			var g = 255-this.x;
 			var b = 0;
 			exports.color(r,g,b);
-			// console.log(Math.round(this.x));
+			console.log(Math.round(this.x));
 		})
 		.onComplete(function(){
 			console.log('finish');
+			gameLoop.stop();
+			_tweener = null;
 		})
 		.start();
-		animator.run();
+
 	},
 	stop : function() {
 		if(_tweener) {
 			_tweener.stop();
 			_tweener = null;
 		}
-		animator.stop();
+		gameLoop.stop();
 	}
 }
